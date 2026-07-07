faae462147261c217f37d5a2c8f90107905cd7ff
Nick Moore <nickmoore83@gmail.com>
2026-07-07 09:03:31 -0400

    Making tweaks and changes.

--- nick_movies.jl
+++ nick_movies.jl
@@ -1,13 +1,26 @@
+#--- GOAL: Make movies of KdV simulations ---#
+
 using Plots, FFTW, LinearAlgebra, Statistics, Printf
 using Parameters, Distributions, Roots, Optim, Random, Test, Distributed, SharedArrays
 using JLD2, DelimitedFiles
 
-# Force GR to run in "headless" mode. This stops the gksqt window 
-# from opening and prevents the freeze.
+# Prevent the gksqt window from freezing the animation.
 ENV["GKSwstype"] = "100"
 
 include("taylor_KdV_solver.jl")
 include("gibbs_sample.jl")
+
+# Set plot defaults.
+function set_plot_defaults()
+	# Set plot default.
+	default(
+	fontfamily = "Times New Roman",
+	titlefont  = 18,  # Title size
+	guidefont  = 14,  # Axis labels (xlabel and ylabel) size
+	tickfont   = 11,  # Axis tick numbers size
+	legendfont = 12   # Legend text size
+	)
+end
 
 # Quick function just to test the sampling.
 function test_sample()
@@ -22,15 +35,7 @@ end
 
 # Function to make a move of the TKdV simulation.
 function make_movie()
-	# Set plot default.
-	default(
-    fontfamily = "Times New Roman",
-    titlefont  = 18,  # Title size
-    guidefont  = 14,  # Axis labels (xlabel and ylabel) size
-    tickfont   = 11,  # Axis tick numbers size
-    legendfont = 12   # Legend text size
-	)
-
+	set_plot_defaults()
 	## Set Parameters
 	K    = 16
 	C2   = 1/120
@@ -52,10 +57,10 @@ function make_movie()
 
 	## Use the sample as the initial condition
 	samp = xdata[:, 1]
-	real_parts = samp[1:K]
-	imag_parts = samp[K+1:end]
 	u0 = zeros(ComplexF64, K+1)
-	u0[2:K+1] = real_parts .+ im .* imag_parts
+	u0[2:K+1] = samp[1:K] .+ im .* samp[K+1:end]
+
+	### CHECK: Should this be a minus sign above?
 
 	# Simple IC
 	#u0 = zeros(ComplexF64, K+1)
@@ -75,37 +80,32 @@ function make_movie()
 	uk_sparse = uk_T[:, idx]
 	uphys_sparse = U_phys_T[:, idx]
 
-    N = 4*(2K+1)
-    dx = 2*pi/N	
+	N = 4*(2K+1)
+	dx = 2*pi/N	
 	xgrid = -pi .+ (0:N-1)*dx
 
+	#--- Print test quauntities ---#
 	println("Initial time: $(t_sparse[1])")
-    #println("Initial u: $(uphys_sparse[:, 1])")
+	#println("Initial u: $(uphys_sparse[:, 1])")
 
-	# Initialize the Canvas
+	#--- Make the simulation movie ---#
+	# Initialize the canvas.
 	plt = plot(xgrid, uphys_sparse[:, 1], 
 				linewidth=2, size=(800, 400), 
 				xlabel="Space (x)", ylabel="u(x,t)", title="TKdV", ylims=(-1.5, 2.5), 
 				label = @sprintf("%.2f", round(t_sparse[1], sigdigits=3) ) )
-
-	# Define the Animation using the pre-built plot
+	# Create the animation.
 	anim = @animate for j in 1:length(t_sparse)
-		# Extract the attributes dictionary for the first series
-    	attrs = plt.series_list[1].plotattributes
-    
-    	# Update data AND the legend label simultaneously
-    	attrs[:y] = uphys_sparse[:, j]
-    	attrs[:label] = @sprintf("%.2f", round(t_sparse[j], sigdigits=3) )
-	    
-	    # We must explicitly call plt at the end of the block so 
-	    # @animate knows what to capture for the frame
-	    plt
+		# Extract the attributes and update them.
+		attrs = plt.series_list[1].plotattributes
+		attrs[:y] = uphys_sparse[:, j]
+		attrs[:label] = @sprintf("%.2f", round(t_sparse[j], sigdigits=3) )
+		plt
 	end
-
-	# 4. Save the Animation
+	# Save the animation.
 	gif(anim, "kdv_movie.gif", fps=15)
 end
 
-#test_sample()
 
 make_movie()
+
